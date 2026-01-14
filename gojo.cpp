4c86b72f2b5ce7f08a3815b9b018c3af2893927e
keerthijeevan <jeevankeerthi259@gmail.com>
2026-01-14 22:16:20 +0530

    random gaussing number

---
+++ gojo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int secret = rand() % 100 + 1;
+    int guess;
+
+    cout << "Guess a number between 1 and 100\n";
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess > secret)
+            cout << "Too high!\n";
+        else if (guess < secret)
+            cout << "Too low!\n";
+        else
+            cout << "🎉 Correct! You guessed it.\n";
+
+    } while (guess != secret);
+
+    return 0;
+}
